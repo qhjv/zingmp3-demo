bc1381614245fbd80cba493b9e01bdc2a9b40791
qhjv <vuquanghuy978@gmail.com>
2022-12-08 21:20:27 +0700

    fix css

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ export const convertTime = (value) => {
   let minutes = ~~(parseFloat(value) / 60);
   let extraSeconds = parseFloat(value) % 60;
 
-  return `${minutes} : ${extraSeconds < 10 ? '0' : ''}${extraSeconds}`;
+  return `${minutes}:${extraSeconds < 10 ? '0' : ''}${extraSeconds}`;
 };
 
 export const isEmptyObject = (obj) => {
